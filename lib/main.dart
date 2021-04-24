0dc595d130e93450f71b10355daa2ae543e63c24
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-04-24 16:00:37 -0300

    Adds `EditorPage`

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:memo_editor/pages/editor_page.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Container(),
-    );
-  }
+  runApp(
+    ProviderScope(
+      child: MaterialApp(
+        title: 'Memo Editor',
+        home: EditorPage(),
+      ),
+    ),
+  );
 }
