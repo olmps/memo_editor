120b460bc134e9caaf62847dadcc5602a7c035ae
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-05-14 17:42:12 -0300

    Updates `CollectionEditorVM`

--- lib/application/view_models/collection_editor_vm.dart
+++ lib/application/view_models/collection_editor_vm.dart
@@ -1,199 +1,289 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:memo_editor/domain/models/collection.dart';
+import 'package:memo_editor/core/faults/errors/inconsistent_state_error.dart';
 import 'package:memo_editor/domain/models/memo.dart';
-import 'package:memo_editor/domain/services/collection_services.dart';
-import 'package:uuid/uuid.dart';
+import 'package:memo_editor/domain/services/memo_services.dart';
 
-const _uuid = Uuid();
+abstract class CollectionEditorVM extends StateNotifier<CollectionEditorState> {
+  CollectionEditorVM(CollectionEditorState state, {required this.collectionId}) : super(state);
 
-class CollectionEditorVM extends StateNotifier<CollectionEditorState> {
-  CollectionEditorVM(Memo initialMemo, {required this.services})
-      : _memos = [initialMemo],
-        _currentRawMemo = initialMemo.question,
-        super(CollectionEditorState(
-          currentMemoIndex: 0,
-          currentRawMemo: initialMemo.question,
-          memosCount: 1,
-          isShowingQuestion: true,
-        ));
+  /// Associated collection id with all of this memos
+  final String collectionId;
 
-  final CollectionServices services;
+  /// The current raw-representation of the selected memo's question or answer
+  ///
+  /// This property must always be kept in sync with any layout changes, because when the state changes, it must have
+  /// the latest data to store it accurately.
+  List<Map<String, dynamic>> get currentRawMemo;
+  set currentRawMemo(List<Map<String, dynamic>> rawMemo);
 
-  final List<Memo> _memos;
-  List<Map<String, dynamic>> _currentRawMemo;
-  List<Map<String, dynamic>> get currentRawMemo => state.currentRawMemo;
+  /// Deletes all memos
+  ///
+  /// Because a collection must always have at least one memo, it creates a blank new - and empty - one.
+  Future<void> clearAllMemos();
+
+  /// Deletes the current selected memo
+  Future<void> deleteCurrentMemo();
+
+  /// Clears all contents from the - currently selected - memo question or answer
+  Future<void> clearCurrentMemoContents();
+
+  /// Switches (or swap) the - currently selected - memo's answer/question
+  ///
+  /// If the question is displaying, swap to the answer, and vice-versa.
+  Future<void> switchCurrentMemoContents();
+
+  /// Updates the selected memo to the one in available in [index]
+  Future<void> updateCurrentMemoIndex(int index);
+
+  /// Creates a new blank memo and selects it as the current one
+  Future<void> addNewMemo();
+}
+
+class CollectionEditorVMImpl extends CollectionEditorVM {
+  CollectionEditorVMImpl(this._memoServices, {required String collectionId})
+      : super(LoadingCollectionEditorState(), collectionId: collectionId) {
+    _loadMemos();
+  }
+
+  final MemoServices _memoServices;
+
+  /// Late-initialized list of memos associated with this [collectionId]
+  late final List<Memo> _memos;
+
+  List<Map<String, dynamic>> _currentRawMemo = [];
+
+  @override
+  List<Map<String, dynamic>> get currentRawMemo => _loadedState.currentRawMemo;
+
+  @override
   set currentRawMemo(List<Map<String, dynamic>> rawMemo) {
     _currentRawMemo = rawMemo;
   }
 
-  void _updatePendingRawChanges() {
-    if (_memos.isEmpty) {
+  /// Returns the current [state] type-casted (forced) to a [LoadedCollectionEditorState]
+  LoadedCollectionEditorState get _loadedState => state as LoadedCollectionEditorState;
+
+  @override
+  Future<void> addNewMemo() async {
+    if (!_assertAndUpdatePendingChanges()) {
       return;
     }
 
-    final currentMemo = _memos[state.currentMemoIndex];
-
-    _memos[state.currentMemoIndex] = currentMemo.copyWith(
-      question: state.isShowingQuestion ? _currentRawMemo : currentMemo.question,
-      answer: !state.isShowingQuestion ? _currentRawMemo : currentMemo.answer,
-    );
-  }
-
-  void addNewMemo() {
-    _updatePendingRawChanges();
-
-    final newMemo = Memo.empty(_uuid.v4());
+    state = _loadedState.copyWith(isUpdating: true);
+    final newMemo = await _memoServices.putPristineMemo(collectionId: collectionId);
     _memos.add(newMemo);
-
     _currentRawMemo = newMemo.question;
-    state = state.copyWith(
+
+    state = _loadedState.copyWith(
       currentMemoIndex: _memos.length - 1,
       memosCount: _memos.length,
       currentRawMemo: _currentRawMemo,
       isShowingQuestion: true,
+      isUpdating: false,
     );
   }
 
-  void updateCurrentMemoIndex(int index) {
-    _updatePendingRawChanges();
+  @override
+  Future<void> updateCurrentMemoIndex(int index) async {
+    if (!_assertAndUpdatePendingChanges()) {
+      return;
+    }
+
+    state = _loadedState.copyWith(isUpdating: true);
+    final currentMemo = _memos[_loadedState.currentMemoIndex];
+    await _memoServices.putMemo(currentMemo, collectionId: collectionId);
 
     final targetMemo = _memos[index];
-
-    _currentRawMemo = state.isShowingQuestion ? targetMemo.question : targetMemo.answer;
-    state = state.copyWith(
+    _currentRawMemo = _loadedState.isShowingQuestion ? targetMemo.question : targetMemo.answer;
+    state = _loadedState.copyWith(
       currentMemoIndex: index,
       currentRawMemo: _currentRawMemo,
+      isUpdating: false,
     );
   }
 
-  void switchCurrentMemoContents() {
-    _updatePendingRawChanges();
+  @override
+  Future<void> switchCurrentMemoContents() async {
+    if (!_assertAndUpdatePendingChanges()) {
+      return;
+    }
 
-    final currentMemo = _memos[state.currentMemoIndex];
+    state = _loadedState.copyWith(isUpdating: true);
+    final currentMemo = _memos[_loadedState.currentMemoIndex];
+    await _memoServices.putMemo(currentMemo, collectionId: collectionId);
 
-    _currentRawMemo = state.isShowingQuestion ? currentMemo.answer : currentMemo.question;
-    state = state.copyWith(
-      isShowingQuestion: !state.isShowingQuestion,
+    // Swaps the content according to what is being currently shown
+    _currentRawMemo = _loadedState.isShowingQuestion ? currentMemo.answer : currentMemo.question;
+    state = _loadedState.copyWith(
+      isShowingQuestion: !_loadedState.isShowingQuestion,
       currentRawMemo: _currentRawMemo,
+      isUpdating: false,
     );
   }
 
-  void clearCurrentMemoContents() {
-    final currentMemo = _memos[state.currentMemoIndex];
+  @override
+  Future<void> clearCurrentMemoContents() async {
+    if (!_assertAndUpdatePendingChanges()) {
+      return;
+    }
 
-    final isShowingQuestion = state.isShowingQuestion;
+    state = _loadedState.copyWith(isUpdating: true);
+
+    final currentMemo = _memos[_loadedState.currentMemoIndex];
+    final isShowingQuestion = _loadedState.isShowingQuestion;
+    // Updates the respective content if showing a question or answer
     final updatedCurrentMemo = currentMemo.copyWith(
       question: isShowingQuestion ? [] : currentMemo.question,
       answer: !isShowingQuestion ? [] : currentMemo.answer,
     );
-    _memos[state.currentMemoIndex] = updatedCurrentMemo;
 
-    _currentRawMemo = <Map<String, dynamic>>[];
-    state = state.copyWith(currentRawMemo: _currentRawMemo);
+    await _memoServices.putMemo(updatedCurrentMemo, collectionId: collectionId);
+
+    _memos[_loadedState.currentMemoIndex] = updatedCurrentMemo;
+    // Also set the current state to reflect this clear operation
+    _currentRawMemo = [];
+    state = _loadedState.copyWith(currentRawMemo: _currentRawMemo, isUpdating: false);
   }
 
-  void deleteCurrentMemo() {
+  @override
+  Future<void> deleteCurrentMemo() async {
     final memosCount = _memos.length;
-    if (memosCount == 1) {
-      _currentRawMemo = <Map<String, dynamic>>[];
-      _memos.clear();
-      addNewMemo();
+    // We don't allow collections having less than a single memo
+    if (!_assertAndUpdatePendingChanges() || memosCount == 1) {
       return;
     }
 
-    final currentIndex = state.currentMemoIndex;
+    state = _loadedState.copyWith(isUpdating: true);
+
+    final currentIndex = _loadedState.currentMemoIndex;
+    await _memoServices.deleteMemoById(_memos[currentIndex].uniqueId);
     _memos.removeAt(currentIndex);
 
     final newCount = _memos.length;
+    // Normalize the next index based on the current position + total updated amount of memos
     final newIndex = currentIndex >= newCount ? newCount - 1 : currentIndex;
     final memoAtIndex = _memos[newIndex];
 
     _currentRawMemo = memoAtIndex.question;
-    state = state.copyWith(
+    state = _loadedState.copyWith(
       currentMemoIndex: newIndex,
       memosCount: newCount,
       currentRawMemo: _currentRawMemo,
       isShowingQuestion: true,
+      isUpdating: false,
     );
   }
 
-  //
-  // Collection Actions
-  //
-
-  void clearCollection() {
-    _currentRawMemo = <Map<String, dynamic>>[];
-    _memos.clear();
-    addNewMemo();
-  }
-
-  Future<void> exportCollection({bool keepingChanges = true}) async {
-    _updatePendingRawChanges();
-
-    // TODO(matuella): Allow metadata changes
-    const tempCollection = Collection(
-      id: 'my_temp_collection',
-      name: 'my_temp_collection',
-      description: 'My temp description of this collection',
-      category: 'TempCategory',
-      tags: ['temp tag 1', 'temp tag 2'],
-      // TODO(matuella): Stitch both collection + memos
-      // memos: _memos,
-    );
-
-    await services.saveCollection(tempCollection);
-    if (!keepingChanges) {
-      clearCollection();
+  @override
+  Future<void> clearAllMemos() async {
+    if (!_assertAndUpdatePendingChanges()) {
+      return;
     }
+
+    state = _loadedState.copyWith(isUpdating: true);
+
+    await _memoServices.deleteAllMemos(collectionId: collectionId);
+    _memos.clear();
+
+    // Add a new blank memo because we don't allow collections without memos
+    final newMemo = await _memoServices.putPristineMemo(collectionId: collectionId);
+    _memos.add(newMemo);
+    _currentRawMemo = newMemo.question;
+
+    state = _loadedState.copyWith(
+      currentMemoIndex: 0,
+      memosCount: 1,
+      currentRawMemo: _currentRawMemo,
+      isShowingQuestion: true,
+      isUpdating: false,
+    );
   }
 
-  Future<void> importCollection() async {
-    final importedCollection = await services.importCollection();
+  Future<void> _loadMemos() async {
+    _memos = await _memoServices.getAllMemos(collectionId: collectionId);
+    const initialIndex = 0;
 
-    _memos..clear();
-    // TODO(matuella): Stitch both collection + memos
-    // ..addAll(importedCollection.memos);
-
-    _currentRawMemo = _memos.first.question;
-    state = state.copyWith(
-      currentMemoIndex: 0,
-      currentRawMemo: _currentRawMemo,
+    state = LoadedCollectionEditorState(
+      currentMemoIndex: initialIndex,
+      currentRawMemo: _memos[initialIndex].question,
       memosCount: _memos.length,
       isShowingQuestion: true,
+      isUpdating: false,
     );
+  }
+
+  /// Asserts for this view model's internal consistency before making any asynchronous changes
+  ///
+  /// The assertion simply checks that the **current loaded state** is not updating anything else, returning `false` if
+  /// it's in fact the middle of some update. If there are no pending updates, syncs the [_currentRawMemo] with its
+  /// respective current memo's question or answer.
+  ///
+  /// Ideally this should be called before making any impactful operation within this view model.
+  ///
+  /// This must only be called when we are sure that the current state is [LoadedCollectionEditorState], throws a
+  /// [InconsistentStateError.viewModel] otherwise.
+  bool _assertAndUpdatePendingChanges() {
+    if (state is! LoadedCollectionEditorState) {
+      throw InconsistentStateError.viewModel(
+          'Trying to run a critical operation before having the collection editor state loaded');
+    }
+
+    // If it's updating, we just return false because we don't want to deal with multiple async operations, as we may
+    // eventually face internal state inconsistency, due to race conditions
+    if (_loadedState.isUpdating) {
+      return false;
+    }
+
+    final currentMemo = _memos[_loadedState.currentMemoIndex];
+
+    _memos[_loadedState.currentMemoIndex] = currentMemo.copyWith(
+      question: _loadedState.isShowingQuestion ? _currentRawMemo : currentMemo.question,
+      answer: !_loadedState.isShowingQuestion ? _currentRawMemo : currentMemo.answer,
+    );
+
+    return true;
   }
 }
 
-class CollectionEditorState with EquatableMixin {
-  CollectionEditorState({
+abstract class CollectionEditorState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class LoadingCollectionEditorState extends CollectionEditorState {}
+
+class LoadedCollectionEditorState extends CollectionEditorState {
+  LoadedCollectionEditorState({
     required this.currentMemoIndex,
     required this.currentRawMemo,
     required this.memosCount,
     required this.isShowingQuestion,
+    required this.isUpdating,
   });
+
   final int currentMemoIndex;
   final List<Map<String, dynamic>> currentRawMemo;
 
   final int memosCount;
   final bool isShowingQuestion;
+  final bool isUpdating;
 
-  CollectionEditorState copyWith({
+  LoadedCollectionEditorState copyWith({
     int? currentMemoIndex,
     List<Map<String, dynamic>>? currentRawMemo,
     int? memosCount,
     bool? isShowingQuestion,
+    bool? isUpdating,
   }) =>
-      CollectionEditorState(
+      LoadedCollectionEditorState(
         currentMemoIndex: currentMemoIndex ?? this.currentMemoIndex,
         currentRawMemo: currentRawMemo ?? this.currentRawMemo,
         memosCount: memosCount ?? this.memosCount,
         isShowingQuestion: isShowingQuestion ?? this.isShowingQuestion,
+        isUpdating: isUpdating ?? this.isUpdating,
       );
 
   @override
-  bool? get stringify => true;
-
-  @override
-  List<Object?> get props => [currentMemoIndex, currentRawMemo, memosCount, isShowingQuestion];
+  List<Object?> get props => [currentMemoIndex, currentRawMemo, memosCount, isShowingQuestion, isUpdating];
 }
