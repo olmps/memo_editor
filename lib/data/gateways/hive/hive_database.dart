ccd40191d802a7eea897806cd39865fe33adf225
Guilherme C. Matuella <guilherme1matuella@gmail.com>
2021-05-14 13:38:27 -0300

    Updates `HiveDatabase` docs

--- lib/data/gateways/hive/hive_database.dart
+++ lib/data/gateways/hive/hive_database.dart
@@ -5,6 +5,7 @@ import 'package:memo_editor/data/gateways/hive/hive_models.dart' as hive_models;
 export 'package:hive/hive.dart' show Box;
 export 'package:hive_flutter/hive_flutter.dart' show BoxX;
 
+///  [Hive] boxes
 enum HiveBox { collections, memos }
 
 extension on HiveBox {
@@ -18,14 +19,19 @@ extension on HiveBox {
   }
 }
 
-/// Initialize all [Hive] dependencies by registering all custom type adapters and pre-load all boxes
+/// Initialize all [Hive] dependencies by registering all custom type adapters
 Future<void> openDatabase() async {
   await Hive.initFlutter();
+  // Register all generated type adapters
   Hive..registerAdapter(hive_models.HiveMemoAdapter())..registerAdapter(hive_models.HiveCollectionAdapter());
 }
 
+/// Exposes a local key-value database using [Hive]
 abstract class HiveDatabase {
-  /// Synchronously retrieve a previously-loaded `Hive` [Box]
+  /// Asynchronously retrieves a `Hive` [Box]
+  ///
+  /// Once a [box] is opened/retrieved with a type [T], this same [T] type must **always** be used for all posterior
+  /// calls, that passes the same [box] argument.
   Future<Box<T>> box<T>(HiveBox box);
 }
 
